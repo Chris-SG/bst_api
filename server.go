d879695ceb3fa1e4b7a3326f1ece571841223d67
Chris-SG <christopher.shandgost@myob.com>
2020-03-18 21:33:48 +1100

    increase timeout

--- server.go
+++ server.go
@@ -45,7 +45,7 @@ func main() {
 		Handler:           r,
 		Addr:		":" + servePort,
 		ReadTimeout: 30 * time.Second,
-		WriteTimeout: 30 * time.Second,
+		WriteTimeout: 90 * time.Second,
 		TLSConfig: &tls.Config{
 			GetCertificate: certManager.GetCertificate,
 		},
