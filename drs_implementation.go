501ae634f1c60edc987f4cd45c48566da3e96030
Chris-SG <christopher.shandgost@myob.com>
2020-04-29 17:30:23 +1000

    Update deps

--- drs_implementation.go
+++ drs_implementation.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/chris-sg/eagate/drs"
 	"github.com/chris-sg/eagate/util"
@@ -16,8 +17,66 @@ func refreshDrsUser(client util.EaClient) (err error) {
 	}
 
 	dancerInfo, err := drs.LoadDancerInfo(client)
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
 	musicData, err := drs.LoadMusicData(client)
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
 	playHist, err := drs.LoadPlayHist(client)
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+
+	d, err := json.MarshalIndent(dancerInfo, "", "  ")
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+	glog.Info(string(d))
+	d, err = json.MarshalIndent(musicData, "", "  ")
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+	glog.Info(string(d))
+	d, err = json.MarshalIndent(playHist, "", "  ")
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+	glog.Info(string(d))
+
+	playerDetails, profileSnapshot, songs, difficulties, playerSongStats, playerScores := drs.Transform(dancerInfo, musicData, playHist)
+
+	d, err = json.MarshalIndent(playerDetails, "", "  ")
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+	glog.Info(string(d))
+	d, err = json.MarshalIndent(profileSnapshot, "", "  ")
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+	glog.Info(string(d))
+	d, err = json.MarshalIndent(songs, "", "  ")
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+	glog.Info(string(d))
+	d, err = json.MarshalIndent(difficulties, "", "  ")
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+	glog.Info(string(d))
+	d, err = json.MarshalIndent(playerSongStats, "", "  ")
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+	glog.Info(string(d))
+	d, err = json.MarshalIndent(playerScores, "", "  ")
+	if err != nil {
+		glog.Errorf("%s\n", err.Error())
+	}
+	glog.Info(string(d))
 
 	glog.Info(dancerInfo, musicData, playHist)
 	return
