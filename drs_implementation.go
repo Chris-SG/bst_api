7f4357a5efe3e91d246aee94e7eeba2e0dcecd23
Chris-SG <christopher.shandgost@myob.com>
2020-04-29 19:58:10 +1000

    Add logging

--- drs_implementation.go
+++ drs_implementation.go
@@ -23,12 +23,18 @@ func refreshDrsUser(client util.EaClient) (err error) {
 
 	playerDetails, profileSnapshot, songs, difficulties, playerSongStats, playerScores := drs.Transform(dancerInfo, musicData, playHist)
 
-	eagate_db.GetDrsDb().AddPlayerDetails(playerDetails)
-	eagate_db.GetDrsDb().AddPlayerProfileSnapshot(profileSnapshot)
-	eagate_db.GetDrsDb().AddSongs(songs)
-	eagate_db.GetDrsDb().AddDifficulties(difficulties)
-	eagate_db.GetDrsDb().AddPlayerSongStats(playerSongStats)
-	eagate_db.GetDrsDb().AddPlayerScores(playerScores)
+	errs := eagate_db.GetDrsDb().AddPlayerDetails(playerDetails)
+	PrintErrors("failed to add player details to db:", errs)
+	errs = eagate_db.GetDrsDb().AddPlayerProfileSnapshot(profileSnapshot)
+	PrintErrors("failed to add player profile snapshot to db:", errs)
+	errs = eagate_db.GetDrsDb().AddSongs(songs)
+	PrintErrors("failed to add songs to db:", errs)
+	errs = eagate_db.GetDrsDb().AddDifficulties(difficulties)
+	PrintErrors("failed to add difficulties to db:", errs)
+	errs = eagate_db.GetDrsDb().AddPlayerSongStats(playerSongStats)
+	PrintErrors("failed to add song stats to db:", errs)
+	errs = eagate_db.GetDrsDb().AddPlayerScores(playerScores)
+	PrintErrors("failed to add player scores to db:", errs)
 
 	glog.Info(dancerInfo, musicData, playHist)
 	return
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/auth0/go-jwt-middleware v0.0.0-20190805220309-36081240882b
 	github.com/chris-sg/bst_server_models v0.0.0-20200421142232-ea5488213431
 	github.com/chris-sg/eagate v0.0.0-20200429081805-518c746036c9
-	github.com/chris-sg/eagate_db v0.0.0-20200429094032-a25be2c88795
+	github.com/chris-sg/eagate_db v0.0.0-20200429095636-74d319e7257c
 	github.com/chris-sg/eagate_models v0.0.0-20200429085721-762005d86979
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
