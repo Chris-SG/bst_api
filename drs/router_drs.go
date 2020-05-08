599e9d674a9565fcee1caba34535c6319ce1be32
Chris-SG <christopher.shandgost@myob.com>
2020-05-08 17:07:31 +1000

    Add table data

--- drs/router_drs.go
+++ drs/router_drs.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/chris-sg/bst_api/common"
 	"github.com/chris-sg/bst_api/utilities"
+	"github.com/chris-sg/eagate_db"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
 	"net/http"
@@ -22,6 +23,9 @@ func CreateDrsRouter() *mux.Router {
 
 	drsRouter.Path("/songs/stats").Handler(utilities.GetProtectionMiddleware().With(
 		negroni.Wrap(http.HandlerFunc(SongStatsGet)))).Methods(http.MethodGet)
+
+	drsRouter.Path("/tabledata").Handler(utilities.GetProtectionMiddleware().With(
+		negroni.Wrap(http.HandlerFunc(TableDataGet)))).Methods(http.MethodGet)
 
 	return drsRouter
 }
@@ -136,5 +140,46 @@ func SongStatsGet(rw http.ResponseWriter, r *http.Request) {
 	bytes, _ := json.Marshal(stats)
 	rw.WriteHeader(http.StatusOK)
 	_, _ = rw.Write(bytes)
+	return
+}
+
+// DrsUpdateUser will load all data provided by the Dance
+// Rush API.
+func TableDataGet(rw http.ResponseWriter, r *http.Request) {
+	users, errMsg, err := common.TryGetEagateUsers(r)
+	if err != nil {
+		status := utilities.WriteStatus("bad", errMsg)
+		bytes, _ := json.Marshal(status)
+		rw.WriteHeader(http.StatusUnauthorized)
+		rw.Write(bytes)
+		return
+	}
+	if len(users) == 0 {
+		status := utilities.WriteStatus("bad", "drs_nouser")
+		bytes, _ := json.Marshal(status)
+		rw.WriteHeader(http.StatusUnauthorized)
+		rw.Write(bytes)
+		return
+	}
+	details, err := retrieveDrsPlayerDetails(users[0].Name)
+	if err != nil {
+		status := utilities.WriteStatus("bad", errMsg)
+		bytes, _ := json.Marshal(status)
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write(bytes)
+		return
+	}
+
+	tableData, errs := eagate_db.GetDrsDb().RetrieveDataForTable(details.Code)
+	if utilities.PrintErrors("failed to retrieve user:", errs) {
+		status := utilities.WriteStatus("bad", "drs_rettbl_err")
+		bytes, _ := json.Marshal(status)
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write(bytes)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+	_, _ = rw.Write([]byte(tableData))
 	return
 }
