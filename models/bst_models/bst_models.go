f5ab3736457bcd2546cd117df29d8143a3f9176c
Chris-SG <christopher.shandgost@myob.com>
2020-05-13 10:55:27 +1000

    Make userid primary for bstprofile

--- models/bst_models/bst_models.go
+++ models/bst_models/bst_models.go
@@ -1,8 +1,8 @@
 package bst_models
 
 type BstProfile struct {
-	User string `json:"user" gorm:"column:user;primary_key"'`
-	UserId int `json:"userid" gorm:"column:user_id; type:serial"`
+	UserId int `json:"userid" gorm:"column:user_id;type:serial;primary_key"`
+	User string `json:"user" gorm:"column:user;unique;not_null"'`
 	Nickname string `json:"nickname" gorm:"column:nickname"`
 	Public bool `json:"public" gorm:"column:public"`
 }
